3800447675a07b0fe8b5be85e6251d09730da2b8
Edinei <galera.org@gmail.com>
2018-12-28 21:11:01 -0200

    salvar arquivo ajuste

--- public/set/save/source.php
+++ public/set/save/source.php
@@ -7,7 +7,7 @@
  * @param array $file
  * @return array
  */
-function addFile(string $column, array $dicionario, array $info, array $file) : array
+function addFile(string $column, array $dicionario, array $info, array $file): array
 {
     $dados['response'] = 1;
 
@@ -16,7 +16,7 @@ function addFile(string $column, array $dicionario, array $info, array $file) : 
         \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads");
 
         foreach ($info["source"] as $type => $ids) {
-            if(!empty($ids)) {
+            if (!empty($ids)) {
                 foreach ($ids as $i) {
                     if ($dicionario[$i]['column'] === $column) {
                         \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "uploads" . DIRECTORY_SEPARATOR . "form");
@@ -26,16 +26,11 @@ function addFile(string $column, array $dicionario, array $info, array $file) : 
                         $nameFile = \Helpers\Check::name(str_replace(".{$extensao}", "", $file['name'])) . "." . strtolower($extensao);
                         $name = "uploads" . DIRECTORY_SEPARATOR . "form" . DIRECTORY_SEPARATOR . date("Y") . DIRECTORY_SEPARATOR . date("m") . DIRECTORY_SEPARATOR . $nameFile;
 
-                        if ($dicionario[$i]["size"] && strlen($name) > $dicionario[$i]["size"]) {
-                            $dados['data'] = "nome do arquivo muito grande";
-                            $dados['response'] = 2;
-                        } else {
-                            if (move_uploaded_file($file['tmp_name'], $name)) {
-                                $dados['data']['url'] = $name;
-                                $dados['data']['type'] = $file['type'];
-                                $dados['data']['size'] = $file['size'];
-                                $dados['data']['name'] = $file['name'];
-                            }
+                        if (move_uploaded_file($file['tmp_name'], $name)) {
+                            $dados['data']['url'] = $name;
+                            $dados['data']['type'] = $file['type'];
+                            $dados['data']['size'] = $file['size'];
+                            $dados['data']['name'] = $file['name'];
                         }
                     }
                 }
@@ -51,11 +46,11 @@ $column = trim(strip_tags(filter_input(INPUT_POST, "column", FILTER_DEFAULT)));
 $dicionario = \Entity\Metadados::getDicionario($entity);
 $info = \Entity\Metadados::getInfo($entity);
 
-if(!empty($_FILES['file']) && !empty($info["source"])) {
+if (!empty($_FILES['file']) && !empty($info["source"])) {
     $data['id'] = "#" . $entity . "-" . $column;
-    if(is_array($_FILES['file']['name'])) {
+    if (is_array($_FILES['file']['name'])) {
         $dados = [];
-        for($i=0;$i< count($_FILES['file']['name']); $i++) {
+        for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
             $file = ["name" => $_FILES['file']['name'][$i], "tmp_name" => $_FILES['file']['tmp_name'][$i], "type" => $_FILES['file']['type'][$i], "error" => $_FILES['file']['error'][$i], "size" => $_FILES['file']['size'][$i]];
             $dados[$i] = addFile($column, $dicionario, $info, $file);
         }
