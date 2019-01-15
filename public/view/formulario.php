f1699817ee6f10c887bb8af2c2f1bac699c4ca65
Edinei <galera.org@gmail.com>
2019-01-15 15:29:02 -0200

    maior espaço

--- public/view/formulario.php
+++ public/view/formulario.php
@@ -1,9 +1,9 @@
 <div class="col font-light theme-d2 theme-border-l margin-bottom" style="border-bottom: solid 5px">
-    <div class="container-900">
+    <div class="container-1200">
         <h4 class="panel padding-32"></h4>
     </div>
 </div>
-<div class="container-900">
+<div class="container-1200">
     <div id="formulario"></div>
 </div>
 
