ee133c8381ac257aedd70a1aae9db25713ba9b6d
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-29 15:27:32 -0300

    form view fix and style fixed

--- public/view/form-maestru/form-maestru.js
+++ public/view/form-maestru/form-maestru.js
@@ -1,3 +1,19 @@
 $(function () {
-    $("#form-maestru").form(PARAM[0], isNumberPositive(PARAM[1]) ? PARAM[1] : null);
+    let entity = PARAM.shift();
+    let id = !isEmpty(PARAM[0]) && isNumberPositive(PARAM[0]) ? PARAM.shift() : null;
+    $("#form-maestru").form(entity, id, !isEmpty(PARAM[0]) ? PARAM : null);
+
+    history.replaceState(
+        {
+            id: id,
+            identificador: Math.floor((Math.random() * 1000)) + "" + Date.now(),
+            route: entity,
+            type: "form",
+            target: "#form-maestru",
+            param: {},
+            scroll: null
+        },
+        null,
+        HOME + (HOME === "" && HOME !== SERVER ? "index.html?url=" : "") + history.state.route
+    );
 })
