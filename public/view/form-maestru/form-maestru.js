43a46f115978ebb55c6b4ec0db670a9c96667ae2
edineijacobbauer <edineibauer@gmail.com>
2021-10-27 08:48:18 -0300

    up navigation

--- public/view/form-maestru/form-maestru.js
+++ public/view/form-maestru/form-maestru.js
@@ -1,20 +1,5 @@
 $(function () {
     const entity = PARAM.shift();
     const id = !isEmpty(PARAM[0]) && isNumberPositive(PARAM[0]) ? PARAM.shift() : null;
-
     $("#form-maestru").form(entity, id, !isEmpty(PARAM[0]) ? PARAM : null);
-
-    history.replaceState(
-        {
-            id: id,
-            identificador: Math.floor((Math.random() * 1000)) + "" + Date.now(),
-            route: entity,
-            type: "form",
-            target: "#form-maestru",
-            param: {},
-            scroll: null
-        },
-        null,
-        HOME + (HOME === "" && HOME !== SERVER ? "index.html?url=" : "") + history.state.route
-    );
 })
