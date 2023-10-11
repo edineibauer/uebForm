e48c2f626de8c73a294aad5e9ec49d7e18a5c4d7
Edinei J. Bauer <galera.org@gmail.com>
2023-10-11 16:29:15 -0300

    check status writable

--- public/src/Form/Status.php
+++ public/src/Form/Status.php
@@ -15,19 +15,25 @@ class Status
      */
     public static function setMensagem(string $entity, string $mensagem, int $user = null)
     {
-        Helper::createFolderIfNoExist(PATH_HOME . "_cdn");
-        Helper::createFolderIfNoExist(PATH_HOME . "_cdn/formSaveStatus");
+        if(is_writable(PATH_HOME)) {
+            Helper::createFolderIfNoExist(PATH_HOME . "_cdn");
+            if(is_writable(PATH_HOME . "_cdn")) {
+                Helper::createFolderIfNoExist(PATH_HOME . "_cdn/formSaveStatus");
+                if(is_writable(PATH_HOME . "_cdn/formSaveStatus")) {
 
-        $userlogin = "";
-        if($user > 0) {
-            $userlogin = $user;
-        } elseif (isset($_SESSION) && !empty($_SESSION["userlogin"]["id"])) {
-            $userlogin = $_SESSION["userlogin"]["id"];
-        }
+                    $userlogin = "";
+                    if ($user > 0) {
+                        $userlogin = $user;
+                    } elseif (isset($_SESSION) && !empty($_SESSION["userlogin"]["id"])) {
+                        $userlogin = $_SESSION["userlogin"]["id"];
+                    }
 
-        if (!empty($userlogin)) {
-            Helper::createFolderIfNoExist(PATH_HOME . "_cdn/formSaveStatus/" . $userlogin);
-            Config::createFile(PATH_HOME . "_cdn/formSaveStatus/" . $userlogin . "/" . $entity . ".txt", $mensagem);
+                    if (!empty($userlogin)) {
+                        Helper::createFolderIfNoExist(PATH_HOME . "_cdn/formSaveStatus/" . $userlogin);
+                        Config::createFile(PATH_HOME . "_cdn/formSaveStatus/" . $userlogin . "/" . $entity . ".txt", $mensagem);
+                    }
+                }
+            }
         }
     }
 }
